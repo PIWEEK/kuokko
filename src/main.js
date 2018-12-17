03e32557772445ffc69271c8e863cf1c4318592d
Andrey Antukh <niwi@niwi.nz>
2018-12-17 12:12:52 +0100

    Initial impl of speech recognition as rxjs.

--- src/main.js
+++ src/main.js
@@ -1,47 +1,53 @@
-import * as _ from "lodash-es";
+import * as sr from "./speechRecognition";
 
-const setup = () => {
-  const recognition = new webkitSpeechRecognition();
-  recognition.lang = 'es-ES'; // sets the language of the current SpeechRecognition 
-  recognition.interimResults = false; // Controls if returns non final results 
-  recognition.continuous = true; // Single or continuous results
-  recognition.maxAlternatives = 1; // Max number of recognized alternatives
-  
-  recognition.onstart = onStart;
-  recognition.onaudiostart = onAudioStart;
-  recognition.onresult = handleResult;
-  recognition.onend = onEnd;
-  recognition.onerror = handleAudioError;
-  
-  recognition.start(); // Starts the speech recognition service listening to incoming audio
-}
+sr.create()
+  .subscribe((event) => {
+    console.log(event);
+  });
 
-/* Fired when the speech recognition service has begun listening to incoming audio */
-const onStart = () => {
-  console.log('Speech recognition service has started');
-}
 
-/* Fired when the user agent has started to capture audio. */
-const onAudioStart = () => {
-  console.log('Audio capturing started');
-}
+// const setup = () => {
+//   const recognition = new webkitSpeechRecognition();
+//   recognition.lang = 'es-ES'; // sets the language of the current SpeechRecognition
+//   recognition.interimResults = false; // Controls if returns non final results
+//   recognition.continuous = true; // Single or continuous results
+//   recognition.maxAlternatives = 1; // Max number of recognized alternatives
 
-/* Fired when the speech recognition service returns a valid result */
-const handleResult = (event) => {
-  console.log(event);
-  for(let caca of event.results) {
-    console.log(caca[0].transcript);
-  }
-}
+//   recognition.onstart = onStart;
+//   recognition.onaudiostart = onAudioStart;
+//   recognition.onresult = handleResult;
+//   recognition.onend = onEnd;
+//   recognition.onerror = handleAudioError;
 
-/* Fired when the speech recognition service has disconnected. */
-const onEnd = () => {
-  console.log('Speech recognition service disconnected');
-}
+//   recognition.start(); // Starts the speech recognition service listening to incoming audio
+// }
 
-/* Fired when a speech recognition error occurs. */
-const handleAudioError = () => {
-  console.error(`Speech recognition error detected: ${event.error}`);
-}
+// /* Fired when the speech recognition service has begun listening to incoming audio */
+// const onStart = () => {
+//   console.log('Speech recognition service has started');
+// }
 
-setup();
+// /* Fired when the user agent has started to capture audio. */
+// const onAudioStart = () => {
+//   console.log('Audio capturing started');
+// }
+
+// /* Fired when the speech recognition service returns a valid result */
+// const handleResult = (event) => {
+//   console.log(event);
+//   for(let caca of event.results) {
+//     console.log(caca[0].transcript);
+//   }
+// }
+
+// /* Fired when the speech recognition service has disconnected. */
+// const onEnd = () => {
+//   console.log('Speech recognition service disconnected');
+// }
+
+// /* Fired when a speech recognition error occurs. */
+// const handleAudioError = () => {
+//   console.error(`Speech recognition error detected: ${event.error}`);
+// }
+
+// setup();
---
+++ src/speechRecognition.js
@@ -0,0 +1,30 @@
+import { Observable } from "rxjs";
+
+export function create() {
+  return new Observable((subscriber) => {
+    console.log("inicialized");
+    const recognition = new webkitSpeechRecognition();
+    recognition.lang = 'es-ES'; // sets the language of the current SpeechRecognition
+    recognition.interimResults = false; // Controls if returns non final results
+    recognition.continuous = true; // Single or continuous results
+    recognition.maxAlternatives = 1; // Max number of recognized alternatives
+
+    let count = 0;
+
+    recognition.onstart = () => {
+      console.log('Speech recognition service has started');
+    };
+
+    recognition.onresult = (event) => {
+      subscriber.next(event.results[count]);
+      count++;
+    };
+
+    recognition.start(); // Starts the speech recognition service listening to incoming audio
+    console.log("kaka");
+
+    return function unsubscribe() {
+      recognition.abort();
+    };
+  });
+};
