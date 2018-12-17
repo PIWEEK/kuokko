b2cd83fd7107ecccc7a1be3c54e3771ce1c8f0cc
Xaviju <xaviju@gmail.com>
2018-12-17 10:36:39 +0100

    Index

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
 import * as _ from "lodash-es";
-console.log(_.concat([1], [2]));
 
+const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+const SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+const SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+const setup = () => {
+  var recognition = new SpeechRecognition();
+  recognition.lang = 'es-ES'; // sets the language of the current SpeechRecognition 
+  recognition.interimResults = false; // Controls if returns non final results 
+  recognition.continuous = true; // Single or continuous results
+  recognition.maxAlternatives = 1;
+
+  recognition.start();
+}
