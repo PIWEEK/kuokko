e71c51d6ba0bba1414c1c1d771911cbc55387364
Andrey Antukh <niwi@niwi.nz>
2018-12-19 17:29:29 +0100

    Movidas.

--- src/js/events.js
+++ src/js/events.js
@@ -1,18 +1,4 @@
-import * as rx from 'rxjs';
-import * as rxop from 'rxjs/operators';
-
-const stream = new rx.BehaviorSubject();
-
 export function emit(type, payload) {
-  stream.next({type, payload});
+  const event = new CustomEvent(type, {detail: payload});
+  document.dispatchEvent(event);
 };
-
-export function subscribe(type, cb) {
-  const sub = stream.pipe(
-    rxop.filter((o) => o.type === type)
-  ).subscribe(cb);
-
-  return () => {
-    sub.unsubscribe();
-  };
-}
