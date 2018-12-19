5f2f37ecdd6c1be22dd4d0eb27f8c2f8bc1bdb7f
Andrey Antukh <niwi@niwi.nz>
2018-12-19 16:57:57 +0100

    Refactor events.

--- src/js/events.js
+++ src/js/events.js
@@ -1,9 +1,18 @@
 import * as rx from 'rxjs';
+import * as rxop from 'rxjs/operators';
 
-const bus = new rx.BehaviorSubject();
-
-export default bus;
+const stream = new rx.BehaviorSubject();
 
 export function emit(type, payload) {
-  bus.next({type, payload});
+  stream.next({type, payload});
 };
+
+export function subscribe(type, cb) {
+  const sub = stream.pipe(
+    rxop.filter((o) => o.type === type)
+  ).subscribe(cb);
+
+  return () => {
+    sub.unsubscribe();
+  };
+}
